b135594ef5da277cc7fb30f70a1a5fd6ba570a03
Shockbob17 <94674990+Shockbob17@users.noreply.github.com>
2023-06-15 18:11:03 +0800

    Trying to organise and setup the project for raw

---
+++ student-frontend/app/first/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/styles.module.css'
+
+function page() {
+    return (
+        <div className={styles.yes}>page</div>
+    )
+}
+
+export default page
