2e64ebb785c3679ef201c89b183fc3b5379ccf56
Shockbob17 <94674990+Shockbob17@users.noreply.github.com>
2023-06-16 11:41:06 +0800

    Creation of new page, second

---
+++ student-frontend/app/second/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../styles/styles.module.css'
+import Stoopidtable from '../components/stoopidtable'
+import FAQheader from '../components/FAQheader'
+import Buttonexample from '../components/button'
+
+
+function page() {
+    return (
+        <div className={styles.main_flex}>
+            <FAQheader></FAQheader>
+        </div>
+    )
+}
+
+export default page
