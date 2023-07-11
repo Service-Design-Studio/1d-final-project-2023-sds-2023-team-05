26a60a540aaa30eb332714a4d26dad22e447f967
Strryke <gavinong2000@gmail.com>
2023-07-11 16:39:05 +0800

    updated shadui components

--- trainer-frontend/components/user-nav.tsx
+++ trainer-frontend/components/user-nav.tsx
@@ -23,16 +23,16 @@ export function UserNav() {
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-9 w-9">
             <AvatarImage src="/avatars/03.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarFallback>BK</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">basil</p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              k@example.com
             </p>
           </div>
         </DropdownMenuLabel>
