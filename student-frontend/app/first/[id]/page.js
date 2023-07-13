95be12dbebbc6a4815d8087c957bf3b2b29d4db2
Shockbob17 <94674990+Shockbob17@users.noreply.github.com>
2023-07-13 15:34:01 +0800

    update changes

---
+++ student-frontend/app/first/[id]/page.js
@@ -0,0 +1,42 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import styles from '../styles/styles.module.css'
+import Stoopidtable from '../components/stoopidtable'
+import FAQheader from '../components/FAQheader'
+import Buttonexample from '../components/button'
+
+
+function Page() {
+    const [classCode, setClassCode] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+
+    useEffect(() => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const code = urlParams.get("classCode");
+
+        if (code) {
+            setClassCode(code);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (classCode) {
+            setIsLoading(false);
+        }
+    }, [classCode]);
+
+    return (
+        <div className={styles.main_flex}>
+            <FAQheader></FAQheader>
+            {!isLoading && (
+                <Stoopidtable classCode={classCode} />
+            )}
+        </div>
+    )
+}
+
+export default Page
