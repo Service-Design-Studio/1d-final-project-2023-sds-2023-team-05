a3c7f7e7476ea539abe2ad7e9c0f4e79840f4c66
LSX99 <shengxianglim99@gmail.com>
2023-07-17 19:39:09 +0800

    commit for ziyi yingyu

---
+++ trainer-frontend/features/flagged-questions.feature
@@ -0,0 +1,8 @@
+Feature: Flagged Questions
+
+    Narrative:
+        As a: trainer who is reviewing the list of questions
+        I want: to be able to check which questions with the most number of flags
+        so that: I can check with the interfaith leaders on the best way to answer the question
+
+    
--- trainer-frontend/features/step_definitions/questions-steps.js
+++ trainer-frontend/features/step_definitions/questions-steps.js
@@ -21,47 +21,54 @@ Then("I should see a button to delete a question", function () {
   // Write code here that turns the phrase above into concrete actions
   return "pending"
 })
-Then(/^I should( not)? see a question with text "([^"]*)"$/, async function (negative, expectedText) {
-  const table = await driver.findElement(By.id("question-table"));
-  const rows = await table.findElements(By.css("tr"));
-  let found = false;
+Then(
+  /^I should( not)? see a question with text "([^"]*)"$/,
+  async function (negative, expectedText) {
+    const table = await driver.findElement(By.id("question-table"))
+    const rows = await table.findElements(By.css("tr"))
+    let found = false
 
-  for (const row of rows) {
-    const texts = await row.findElements(By.css("span"));
+    for (const row of rows) {
+      const texts = await row.findElements(By.css("span"))
 
-    for (const text of texts) {
-      const questionText = await text.getText();
+      for (const text of texts) {
+        const questionText = await text.getText()
 
-      if (questionText.includes(expectedText)) {
-        found = true;
-        break;
+        if (questionText.includes(expectedText)) {
+          found = true
+          break
+        }
+      }
+
+      if (found) {
+        break
       }
     }
 
-    if (found) {
-      break;
+    if (!negative) {
+      expect(found).to.be.false
+    } else {
+      expect(found).to.be.true
     }
   }
+)
 
-  if (negative) {
-    expect(found).to.be.false;
-  } else {
-    expect(found).to.be.true;
-  }
-});
+Then(
+  /^I open the menu for the question of text "([^"]*)"$/,
+  async function (expectedText) {
+    const table = await driver.findElement(By.id("question-table"))
+    const rows = await table.findElements(By.css("tr"))
 
-Then(/^I open the menu for the question of text "([^"]*)"$/, async function (expectedText) {
-  const table = await driver.findElement(By.id("question-table"));
-  const rows = await table.findElements(By.css("tr"));
-
-  for (const row of rows) {
-    const text = await row.findElement(By.css("span"));
-    const questionText = await text.getText();
-    if (questionText.includes(expectedText)) {
-      const button = await row.findElement(By.xpath(".//button[.//span[text()='Open menu']]"));
-      await button.click();
-      break;
+    for (const row of rows) {
+      const text = await row.findElement(By.css("span"))
+      const questionText = await text.getText()
+      if (questionText.includes(expectedText)) {
+        const button = await row.findElement(
+          By.xpath(".//button[.//span[text()='Open menu']]")
+        )
+        await button.click()
+        break
+      }
     }
   }
-
-})
+)
