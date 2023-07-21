a1b92725cf8cb82958bba97291cdac6d13200a04
LSX99 <shengxianglim99@gmail.com>
2023-07-21 16:36:43 +0800

    cucumber report out for learner session

--- tests/features/step-definitions/chatbot.js
+++ tests/features/step-definitions/chatbot.js
@@ -2,51 +2,34 @@ const { Given, When, Then, And } = require('@cucumber/cucumber');
 const { By } = require('selenium-webdriver');
 const { expect, assert } = require('chai');
 
-/////////////////////////////// Learner sees a customised FAQ page ////////////////////////////
-Given('I am on the sign in page', async function () {
-	const h2Element = await driver.findElement(
-		By.xpath('/html/body/main/div/h2')
-	);
-	const pageTitle = await h2Element.getText();
-	assert.strictEqual(pageTitle, 'Kampung Klass');
-});
-
-// Function to run the next line
 async function keyInDigit(sessionCode, digitIndex) {
 	const digit = sessionCode[digitIndex];
 	const box = await driver.findElement(By.id(digitIndex.toString()));
 	await box.sendKeys(digit);
 }
 
-When(/^I key in the class code (.*)/, async function (sessionCode) {
-	await keyInDigit(sessionCode, 0); // key in first digit
-	await keyInDigit(sessionCode, 1); // key in second digit
-	await keyInDigit(sessionCode, 2); // key in third digit
-	await keyInDigit(sessionCode, 3); // key in fourth digit
-	await keyInDigit(sessionCode, 4); // key in fifth digit
-	await keyInDigit(sessionCode, 5); // key in sixth digit
-});
+/////////////////////////////// Learner sees a customised FAQ page ////////////////////////////
 
-Then(
-	'I will see the customised FAQ page with the following questions:',
-	async function (dataTable) {
-		// Get the list of expected questions from the Cucumber table
-		const expectedQuestions = dataTable.raw().flat();
+// Then(
+// 	'I will see the customised FAQ page with the following questions:',
+// 	async function (dataTable) {
+// 		// Get the list of expected questions from the Cucumber table
+// 		const expectedQuestions = dataTable.raw().flat();
 
-		// Assuming you have located the FAQ questions on the page, you can retrieve them and verify
-		const actualQuestions = await driver.findElements(By.id('faq-question'));
+// 		// Assuming you have located the FAQ questions on the page, you can retrieve them and verify
+// 		const actualQuestions = await driver.findElements(By.id('faq-question'));
 
-		// Extract the text from the actual questions
-		const actualQuestionsText = await Promise.all(
-			actualQuestions.map(async (element) =>
-				element.innerText.trim().split('\n')[1].trim()
-			)
-		);
+// 		// Extract the text from the actual questions
+// 		const actualQuestionsText = await Promise.all(
+// 			actualQuestions.map(async (element) =>
+// 				element.innerText.trim().split('\n')[1].trim()
+// 			)
+// 		);
 
-		// Compare the expected and actual questions
-		expect(actualQuestionsText).to.deep.equal(expectedQuestions);
-	}
-);
+// 		// Compare the expected and actual questions
+// 		expect(actualQuestionsText).to.deep.equal(expectedQuestions);
+// 	}
+// );
 
 /////////////////////////////// Learner sees the chatbot page ////////////////////////////
 Given('I am on the customised FAQ page', async function () {
