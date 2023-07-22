10f7a2f3e49f895396260d52322e5ab4b089ecfe
Strryke <gavinong2000@gmail.com>
2023-07-22 18:41:39 +0800

    backend unit testing models n controllers

--- backend/faqapi/test/models/chat_test.rb
+++ backend/faqapi/test/models/chat_test.rb
@@ -1,7 +1,22 @@
-require "test_helper"
+# test/models/chat_test.rb
+
+require 'test_helper'
 
 class ChatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with valid attributes" do
+    chat = Chat.new(flagged: true, question: "Sample Question", answer: "Sample Answer")
+    assert chat.valid?
+  end
+
+  test "should be invalid without a question" do
+    chat = Chat.new(flagged: true, answer: "Sample Answer")
+    assert_not chat.valid?
+    assert_includes chat.errors[:question], "can't be blank"
+  end
+
+  test "should be invalid without an answer" do
+    chat = Chat.new(flagged: true, question: "Sample Question")
+    assert_not chat.valid?
+    assert_includes chat.errors[:answer], "can't be blank"
+  end
 end
--- backend/faqapi/test/models/session_test.rb
+++ backend/faqapi/test/models/session_test.rb
@@ -1,7 +1,38 @@
-require "test_helper"
+# test/models/session_test.rb
+
+require 'test_helper'
 
 class SessionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid with valid attributes" do
+    session = Session.new(title: "Sample Session", author: "John Doe")
+    assert session.valid?
+  end
+
+  test "should be invalid without a title" do
+    session = Session.new(author: "John Doe")
+    assert_not session.valid?
+    assert_includes session.errors[:title], "can't be blank"
+  end
+
+  test "should be invalid without an author" do
+    session = Session.new(title: "Sample Session")
+    assert_not session.valid?
+    assert_includes session.errors[:author], "can't be blank"
+  end
+
+  test "should generate a classcode before creation" do
+    session = Session.new(title: "Sample Session", author: "John Doe")
+    assert_nil session.classcode
+    session.save
+    assert_not_nil session.classcode
+  end
+
+  test "should have a unique classcode" do
+    # all session have different classcodes
+    session1 = Session.new(title: "Sample Session", author: "John Doe")
+    session2 = Session.new(title: "Sample Session", author: "John Doe")
+    session1.save
+    session2.save
+    assert_not_equal session1.classcode, session2.classcode
+  end
 end
