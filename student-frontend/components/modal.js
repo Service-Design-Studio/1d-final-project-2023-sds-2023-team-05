3d3c74cb0cf5bc0a68286f97107611d7cc0b082c
Shockbob17 <94674990+Shockbob17@users.noreply.github.com>
2023-07-24 15:48:35 +0800

    Adding the selection of the reason to modal

--- student-frontend/components/modal.js
+++ student-frontend/components/modal.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from '@/styles/modal.module.css';
 
 function Modal({ isOpen, onClose, children, messageId, setIsModalOpen, API_URL }) {
+    const [selectedReason, setSelectedReason] = useState('');
+    const [othersResponse, setOthersResponse] = useState('');
+
 
     async function flagComment(id, setIsModalOpen, API_URL) {
+        if (selectedReason == '') {
+            alert('Select a reason');
+            return
+        }
         const res = await fetch(`${API_URL}/chats/${id}`, {
             body: JSON.stringify({
                 flagged: true,
+                reason: selectedReason,
+                comment: othersResponse
             }),
             headers: {
                 'Content-Type': 'application/json',
@@ -21,6 +31,13 @@ function Modal({ isOpen, onClose, children, messageId, setIsModalOpen, API_URL }
         }
     }
 
+    const handleReasonClick = (reason) => {
+        setSelectedReason(reason);
+        if (reason !== 'Others') {
+            setOthersResponse('');
+        }
+    };
+
     return (
         <>
             {isOpen && (
@@ -30,8 +47,53 @@ function Modal({ isOpen, onClose, children, messageId, setIsModalOpen, API_URL }
                             <button className={styles.closeButton} onClick={onClose}>
                                 <span aria-hidden="true">&times;</span>
                             </button>
-                            {children}
-                            <button className={styles.flagButton} onClick={() => flagComment(messageId, setIsModalOpen, API_URL)}>flag</button>
+                            <div style={{ paddingTop: '20px', marginBottom: '10px' }}>
+                                <h2>Why are you flagging this answer?</h2>
+                            </div>
+                            <div
+                                className={`${styles.reasonbox} ${selectedReason === 'Rude or Offensive' ? styles.selected : ''
+                                    }`}
+                                onClick={() => handleReasonClick('Rude or Offensive')}
+                            >
+                                Rude or Offensive
+                            </div>
+                            <div
+                                className={`${styles.reasonbox} ${selectedReason === 'Misinformation' ? styles.selected : ''
+                                    }`}
+                                onClick={() => handleReasonClick('Misinformation')}
+                            >
+                                Misinformation
+                            </div>
+                            <div
+                                className={`${styles.reasonbox} ${selectedReason === 'Does not answer the question' ? styles.selected : ''
+                                    }`}
+                                onClick={() => handleReasonClick('Does not answer the question')}
+                            >
+                                Does not answer the question
+                            </div>
+                            <div
+                                className={`${styles.reasonbox} ${selectedReason === 'Religious Propaganda' ? styles.selected : ''
+                                    }`}
+                                onClick={() => handleReasonClick('Religious Propaganda')}
+                            >
+                                Religious Propaganda
+                            </div>
+                            <div
+                                className={`${styles.reasonbox} ${selectedReason === 'Others' ? styles.selected : ''
+                                    }`}
+                                onClick={() => handleReasonClick('Others')}
+                            >
+                                Others
+                            </div>
+                            {selectedReason === 'Others' && (
+                                <textarea
+                                    className={styles.textarea}
+                                    value={othersResponse}
+                                    onChange={(e) => setOthersResponse(e.target.value)}
+                                    placeholder="Please provide your reason..."
+                                />
+                            )}
+                            <button className={styles.flagButton} onClick={() => flagComment(messageId, setIsModalOpen, API_URL)}>Flag Question</button>
                         </div>
                     </div>
                 </div>
