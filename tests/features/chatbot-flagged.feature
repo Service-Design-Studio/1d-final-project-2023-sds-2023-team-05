fe40048ba5f4065cb077c418d8d267b9dcf8acf1
Soh Ying Yu <sohyingyu19@gmail.com>
2023-07-27 00:57:01 +0800

    faq by session (other files are commented out)

--- tests/features/chatbot-flagged.feature
+++ tests/features/chatbot-flagged.feature
@@ -1,15 +1,15 @@
-Feature: Chatbot 
+# Feature: Chatbot 
 
-    Narrative
-        As a: trainer who is reviewing the chatbot prompts and answers
-        I want: to be able to see the flagged answers by students
-        so that: I can review the answers with interfaith subject matter experts to get an accurate answer for that prompt
+#     Narrative
+#         As a: trainer who is reviewing the chatbot prompts and answers
+#         I want: to be able to see the flagged answers by students
+#         so that: I can review the answers with interfaith subject matter experts to get an accurate answer for that prompt
 
-    @Trainer
-    Scenario: Trainer heads to Chatbot page
-        Given I am on the questions page
-        When I click on the Chatbot button in the header
-        Then I will be on the chatbot page
+#     @Trainer
+#     Scenario: Trainer heads to Chatbot page
+#         Given I am on the questions page
+#         When I click on the Chatbot button in the header
+#         Then I will be on the chatbot page
     
-    @Trainer
-    Scenario: Trainer heads to the All tab to see all the questions
+#     @Trainer
+#     Scenario: Trainer heads to the All tab to see all the questions
--- tests/features/step-definitions/faq-by-session.js
+++ tests/features/step-definitions/faq-by-session.js
@@ -1,99 +1,108 @@
-const { Given, When, Then, And } = require('@cucumber/cucumber');
-const { By } = require('selenium-webdriver');
-const { expect, assert } = require('chai');
+// const { Given, When, Then, And } = require('@cucumber/cucumber');
+// const { By } = require('selenium-webdriver');
+// const { expect, assert } = require('chai');
 
-// function: Key in session code
-async function keyInDigit(sessionCode, digitIndex) {
-	const digit = sessionCode[digitIndex];
-	const box = await driver.findElement(By.id(digitIndex.toString()));
-	await box.sendKeys(digit);
-}
+// // function: Key in session code
+// async function keyInDigit(sessionCode, digitIndex) {
+// 	const digit = sessionCode[digitIndex];
+// 	const box = await driver.findElement(By.id(digitIndex.toString()));
+// 	await box.sendKeys(digit);
+// }
 
-///// Learner sees a customised FAQ page for the training session with code 952555
-Given('I am on the sign in page', async function () {
-	const h2Element = await driver.findElement(
-		By.xpath('/html/body/main/div/h2')
-	);
-	const pageTitle = await h2Element.getText();
-	assert.strictEqual(pageTitle, 'Kampung Klass');
-});
+// Given('I am on the sign in page', async function () {
+// 	const h2Element = await driver.findElement(By.xpath('/html/body/main/div/h2'));
+// 	const pageTitle = await h2Element.getText();
+// 	assert.strictEqual(pageTitle, 'Kampung Klass');
+// });
 
-When(/^I key in the class code (.*)/, async function (sessionCode) {
-	await keyInDigit(sessionCode, 0); // key in first digit
-	await keyInDigit(sessionCode, 1); // key in second digit
-	await keyInDigit(sessionCode, 2); // key in third digit
-	await keyInDigit(sessionCode, 3); // key in fourth digit
-	await keyInDigit(sessionCode, 4); // key in fifth digit
-	await keyInDigit(sessionCode, 5); // key in sixth digit
-});
+// When(/^I key in the class code (.*)$/, async function (sessionCode) {
+// 	// Convert the sessionCode to string
+// 	sessionCode = sessionCode.toString();
 
-Then(
-	'I will see the customised FAQ page with the following questions:',
-	async function (dataTable) {
-		await driver.sleep(1000);
-		const questions = await driver.findElements(By.className('question'));
+// 	// Iterate through each digit in the sessionCode and key it in
+// 	for (let i = 0; i < sessionCode.length; i++) {
+// 		await keyInDigit(sessionCode, i);
+// 	}
+// });
 
-		const questionTexts = await Promise.all(
-			questions.map(async (question) => {
-				return await question.getText();
-			})
-		);
+// Then(
+// 	'I will see the customised FAQ page with the following questions:',
+// 	async function (dataTable) {
+//   // Wait for a short period to allow the page to load properly
+//   		await driver.sleep(1000);
 
-		const dataTableTexts = dataTable.raw().flat();
+//   // Get all the questions from the customised FAQ page
+//   		const questions = await driver.findElements(By.className('question'));
 
-		for (data of dataTableTexts) {
-			expect(questionTexts.includes(data)).to.be.true;
-		}
-	}
-);
+//   // Get the text of each question and store them in questionTexts array
+//   		const questionTexts = await Promise.all(
+//     		questions.map(async (question) => {
+//       			return await question.getText();
+//     		})
+//   		);
+
+//   // Flatten the dataTable rows into a single array
+//   		const dataTableTexts = dataTable.raw().flat();
+
+
+//   // Compare each question from the dataTable with the questions on the page
+//   		for (const data of dataTableTexts) {
+//     		expect(questionTexts.includes(data)).to.be.true;
+//   		}
+// 	}
+// );
+
+
 
 // ///// Learner sees a different customised FAQ page for the training session with code 725018
 
 ///// Learner clicks into a question of interest to see the answer
-Given(
-	/^I am on the customised FAQ page for session code (.*)/,
-	async function (sessionCode) {
-		await keyInDigit(sessionCode, 0); // key in first digit
-		await keyInDigit(sessionCode, 1); // key in second digit
-		await keyInDigit(sessionCode, 2); // key in third digit
-		await keyInDigit(sessionCode, 3); // key in fourth digit
-		await keyInDigit(sessionCode, 4); // key in fifth digit
-		await keyInDigit(sessionCode, 5); // key in sixth digit
+// Given('I am on the customised FAQ page for session code', async function () {
+// 	await keyInDigit('725018', 0); // key in first digit
+// 	await keyInDigit('725018', 1); // key in second digit
+// 	await keyInDigit('725018', 2); // key in third digit
+// 	await keyInDigit('725018', 3); // key in fourth digit
+// 	await keyInDigit('725018', 4); // key in fifth digit
+// 	await keyInDigit('725018', 5); // key in sixth digit
 
-		// await driver.sleep(1000);
-	}
-);
+// 	await driver.sleep(1000);
+// 	}
+// );
 
-When(/^I type "(.*)" in the search bar/, async function (search) {
-	return true;
-});
+// When(/^I click the question with text'(.*)'/, 
+// async function () {
+// 	const firstQuestion = await driver.findElement(By.id('table_text'));
+// 	await firstQuestion.click();
+// });
 
-Then(/^I will see a question with text "(.*)"/, async function (search) {
-	return true;
-});
+// Then(/^I will see the corresponding answer with text "(.*)"/, 
+// async function () {
+// 	const firstAnswer = await driver.findElement(By.id('table_answer'));
+// 	await firstAnswer.click();
+// });
 
-///// Learner successfully searches for a question regarding pilgrimage in the search bar for session code 725018
-When(/^I click the question with text "(.*)"/, async function (question) {
-	return true;
-});
+// ///// Learner successfully searches for a question regarding pilgrimage in the search bar for session code 725018
+// When(/^I click the question with text "(.*)"/, async function (question) {
+// 	return true;
+// });
 
-Then(
-	/^I will see the corresponding answer with text "(.*)"/,
-	async function (answer) {
-		return true;
-	}
-);
+// Then(
+// 	/^I will see the corresponding answer with text "(.*)"/,
+// 	async function (answer) {
+// 		return true;
+// 	}
+// );
 
-///// Learner fails to search for a question regarding pilgrimage in the search bar for session code 952255
-Then('I will see nothing on the screen', async function () {
-	return true;
-});
+// ///// Learner fails to search for a question regarding pilgrimage in the search bar for session code 952255
+// Then('I will see nothing on the screen', async function () {
+// 	return true;
+// });
 
-///// Learner goes back to landing page from the FAQ page
-When('I click the back button', async function () {
-	return true;
-});
+// ///// Learner goes back to landing page from the FAQ page
+// When('I click the back button', async function () {
+// 	return true;
+// });
 
-Then(/^I will see the landing page titled "(.*)"/, async function (pageTitle) {
-	return true;
-});
+// Then(/^I will see the landing page titled "(.*)"/, async function (pageTitle) {
+// 	return true;
+// });
